1357a52c8c145b2b76b00eb7cebac30e2bc328c8
Sune Jakobsson <sune.jakobsson@comoyo.com>
2014-03-24 22:31:38 +0100

    Adding details and filters

---
+++ src/main/java/com/ntnu/sune/resource/MonitoringResponseFilter.java
@@ -0,0 +1,38 @@
+package com.ntnu.sune.resource;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Sune Jakobsson,
+ * @version  Mar 2014
+ */
+public class MonitoringResponseFilter implements ContainerResponseFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(MonitoringResponseFilter.class.getName());
+
+    static Integer cnt = new Integer(0);
+
+    int getCount() {
+
+        return cnt;
+    }
+
+    @Override
+    public ContainerResponse filter(ContainerRequest request, ContainerResponse response){
+
+        if (response.getStatus() < (Response.Status.MOVED_PERMANENTLY.getStatusCode()-2) && response.getStatus() >= Response.Status.OK.getStatusCode()) {
+
+            synchronized (cnt){
+                cnt++;
+            }
+            LOGGER.log(Level.ALL, cnt.toString());
+        }
+        return response;
+    }
+}
