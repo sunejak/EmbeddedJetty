d653fbd9b465b706380dbe97243fb25bcdc69ce3
Sune Jakobsson <sune.jakobsson@comoyo.com>
2014-09-05 21:22:33 +0200

    Tweak plot script

--- src/main/scripts/Gnuplot_all.sh
+++ src/main/scripts/Gnuplot_all.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #
-# Use "GnuPlot" show all data.
+# Use "GnuPlot" to show all data.
 #
-
 logfile=$1
 if [[ ${logfile} == "" ]] ; then
     echo "Usage: logfile timezone"
@@ -11,20 +10,17 @@ fi
 
 timezone=$2
 if [[ ${timezone} == "" ]] ; then
-
     # extract timezone from logfile
     timezone=`tail -10 ${logfile} | grep Invoke | sed "s:  : :g" | cut -d " " -f15 | tail -1`
     echo "Setting timezone to $timezone found from ${logfile}"
 fi
 
+secondaryPattern=$3
 #
-# find the list of unique entries in log file.
+# Find the list of unique entries in log file.
 #
-
-for names in `tail -1000 ${logfile} | grep Invoke_HTTP_Get | cut -d " " -f7 | sed "s%http://ntnu-test.%%g" | sed "s%:8090/token/ReceiveToken%%g" | sort | uniq`
+for names in `tail -500 ${logfile} | grep Invoke_HTTP_Get | cut -d " " -f7 | sed "s%http://ntnu-test.%%g" | sed "s%:8090/token/ReceiveToken%%g" | sort | uniq`
 do
-
 echo Plotting ${names}
-./Generate_plot.sh ${logfile} ${names} ${timezone}
-
+./Generate_plot.sh ${logfile} ${names} ${timezone} ${secondaryPattern}
 done
