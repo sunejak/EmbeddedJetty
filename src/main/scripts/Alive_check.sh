39b4cbbce8a837e387c755323b8ff73cf15292ad
Sune Jakobsson <sune.jakobsson@comoyo.com>
2014-10-12 08:24:42 +0200

    experiment update

--- src/main/scripts/Alive_check.sh
+++ src/main/scripts/Alive_check.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 #
-#
 
-curl http://ntnu-test.tullinloekka.uninett.simula.nornet:8090/token/ReceiveToken 
-# curl http://ntnu-test.tullinloekka.hafslund.simula.nornet:8090/token/ReceiveToken 
-# curl http://ntnu-test.tullinloekka.telenor.simula.nornet:8090/token/ReceiveToken 
-curl http://ntnu-test.furuset.uninett.uio.nornet:8090/token/ReceiveToken 
-# curl http://ntnu-test.furuset.powertech.uio.nornet:8090/token/ReceiveToken 
-curl http://ntnu-test.aunegaarden.uninett.uit.nornet:8090/token/ReceiveToken 
-# curl http://ntnu-test.aunegaarden.telenor.uit.nornet:8090/token/ReceiveToken 
-# curl http://ntnu-test.aunegaarden.powertech.uit.nornet:8090/token/ReceiveToken 
-curl http://ntnu-test.rennesoey.uninett.uis.nornet:8090/token/ReceiveToken 
-# curl http://ntnu-test.rennesoey.powertech.uis.nornet:8090/token/ReceiveToken 
-curl http://ntnu-test.midnattssol.uninett.unis.nornet:8090/token/ReceiveToken 
-curl http://ntnu-test.kettwig.dfn.ude.nornet:8090/token/ReceiveToken 
-# curl http://ntnu-test.kettwig.versatel.ude.nornet:8090/token/ReceiveToken
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.tullinloekka.uninett.simula.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.tullinloekka.hafslund.simula.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.tullinloekka.telenor.simula.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.furuset.uninett.uio.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.furuset.powertech.uio.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.aunegaarden.uninett.uit.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.aunegaarden.telenor.uit.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.aunegaarden.powertech.uit.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.rennesoey.uninett.uis.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.rennesoey.powertech.uis.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.midnattssol.uninett.unis.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.kettwig.dfn.ude.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  http://ntnu-test.kettwig.versatel.ude.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
 
+
+
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.aunegaarden.uninett.uit.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.aunegaarden.telenor.uit.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.aunegaarden.powertech.uit.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.kettwig.dfn.ude.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.kettwig.versatel.ude.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.vaenern.sunet.kau.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.floeibanen.uninett.uib.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.floeibanen.bkk.uib.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.furuset.uninett.uio.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.furuset.powertech.uio.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.tullinloekka.uninett.simula.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.tullinloekka.hafslund.simula.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
+curl --max-time 3  --write-out "Took: %{time_total} ms %{http_code} %{url_effective}\n"  ntnu-test.tullinloekka.telenor.simula.nornet:8090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S`
