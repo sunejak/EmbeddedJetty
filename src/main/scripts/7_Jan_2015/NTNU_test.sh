ec71e1165d50d0e5dc58fdf13d95fcc03c5640ca
Sune Jakobsson <sune.jakobsson@comoyo.com>
2015-01-08 19:36:29 +0100

    Final NTNU update

--- src/main/scripts/7_Jan_2015/NTNU_test.sh
+++ src/main/scripts/7_Jan_2015/NTNU_test.sh
@@ -8,3 +8,18 @@
 curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.heimdal.uninett.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
 curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.heimdal.powertech.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
 
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.bymarka.uninett.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.bymarka.powertech.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.lerkendal.uninett.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.lerkendal.powertech.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.byaasen.uninett.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.byaasen.powertech.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.bakklandet.uninett.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.bakklandet.powertech.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.bybro.uninett.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+curl --max-time 9  --write-out "Took: %{time_total} s %{time_connect} s %{time_namelookup} s %{http_code} %{url_effective}\n"  ntnu-test.bybro.powertech.ntnu.nornet:9090/token/ReceiveToken?Alive_Check=`date +%H_%M_%S_%N`
+
